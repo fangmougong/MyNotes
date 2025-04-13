790ed7d54af7c319024ee0dee3be52e9989ae6ed
fangmougong <1776071373@qq.com>
2025-04-13 16:47:56 +0800

    initial commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/MyNotes/',
   bundler: viteBundler(),
   theme: defaultTheme({
     navbar: [
